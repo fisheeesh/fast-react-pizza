d456cb86ce6109ab674a1412a7ff51650483250f
fisheeesh <swanphyo444@gmail.com>
2025-03-21 17:35:41 +0700

    update cart quantity

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,15 +4,16 @@ import Button from '../../ui/Button'
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice'
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from '../cart/UpdateItemQuantity'
 
 /* eslint-disable react/prop-types */
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch()
 
-  const currentQuantitiy = useSelector(getCurrentQuantityById(id))
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
 
-  const isInCart = currentQuantitiy > 0
+  const isInCart = currentQuantity > 0
 
   const onHandleClick = () => {
     let newItem = {
@@ -35,7 +36,12 @@ function MenuItem({ pizza }) {
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm ">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart &&
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity pizzaId={id} currentQuantity={currentQuantity} />
+              <DeleteItem pizzaId={id} />
+            </div>
+          }
           {!soldOut && !isInCart && <Button type="small" onClick={onHandleClick}>Add to cart</Button>}
         </div>
       </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,6 +6,7 @@ export default function Button({ children, disabled, to, type, onClick }) {
     const styles = {
         primary: base + ' px-4 py-3 md:px-6 md:py-4',
         small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+        round: base + ' px-2 py-1 text-xs',
         secondary: "uppercase text-sm border-2 border-stone-300 px-4 py-2.5 md:px-6 md:py-3.5 bg-transparent font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:text-stone-800 hover:bg-stone-300 focus:outline-0 focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 disabled:cursor-not-allowed focus:ring-offset-2 cursor-pointer transition-colors duration-300 disabled:bg-slate-600"
     }
 
